8d2b90ca278ce8a4f1d875d49410039ff153e2de
Qwarkeh <christine.f.stark@gmail.com>
2022-01-31 16:17:21 -0500

    added mouseover eventto change squares  to black

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const container = document.querySelector('div');
+createBoard();
 
 function createRow(startingClass) {
+    const container = document.querySelector('div');
+
     const row = document.createElement('div')
     row.classList.add('row');
     container.appendChild(row);
@@ -10,10 +12,18 @@ function createRow(startingClass) {
             const newClass = startingClass + i;
 
             const square = document.createElement('div');
+            square.classList.add('square');
             square.classList.add(`sq-${newClass}`);
             square.textContent = `${newClass}`
             row.appendChild(square);
-            }
+
+            //square.onclick = () => square.style.background = 'black';
+
+            square.addEventListener('mouseover', event => {
+                console.log('enter');
+                square.style.background = 'black';
+            });
+        }
 };
 
 function createBoard() {
@@ -22,7 +32,6 @@ function createBoard() {
     for (let i = 0; i < 16; i++) {
         createRow(startModifer);
         startModifer += 16;
-    }
+    };
 };
 
-createBoard();
