6f9916d1bbf783f8bf9f291546de64faa008616c
Qwarkeh <christine.f.stark@gmail.com>
2022-01-31 23:17:23 -0500

    added styling and coded reset button's event handler

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-createBoard(6);
+createBoard(16);
 reset();
 
 function createRow(width) {
-    const container = document.querySelector('div');
+    const container = document.querySelector('.container');
 
     const row = document.createElement('div')
     row.classList.add('row');
@@ -14,23 +14,37 @@ function createRow(width) {
             row.appendChild(square);
 
             square.addEventListener('mouseover', event => {
-                square.style.background = 'black';
-            });
+                square.style.background = 'white';
+            })
         }
-};
+}
 
 function createBoard(width) {
     for (let i = 0; i < width; i++) {
         createRow(width);
-    };
-};
+    }
+}
 
 function reset() {
-    
     const reset = document.querySelector('#reset');
 
     reset.addEventListener('click', () => {
-        console.log('clicked');
+        while (true) {
+            let userInput = parseInt(prompt('How many rows do you want to have? (Pick an integer from 1 to 100)'));
 
-    });
+            if (isNaN(userInput) === true) {
+                return;
+            }
+            else if (typeof userInput !== 'number' || userInput > 100 || userInput <= 0) {
+                alert('Pick a valid integer');
+            }    
+             else {
+                const removeRows = document.querySelectorAll('.row');
+                for (i = 0; i < removeRows.length; i++) {
+                    removeRows[i].remove();
+            }
+               return createBoard(userInput);
+            }
+        } 
+    })
 }
